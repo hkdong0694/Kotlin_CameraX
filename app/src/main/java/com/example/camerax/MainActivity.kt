d1542ad893a75cdc6a9fa1d039633c92b1de2700
한경동 <hkd0694@gmail.com>
2021-06-22 21:48:07 +0900

    카메라 API 주석 추가

--- app/src/main/java/com/example/camerax/MainActivity.kt
+++ app/src/main/java/com/example/camerax/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.camerax
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,13 +10,15 @@ import android.view.View
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
-import java.util.ArrayList
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -48,7 +51,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_camera -> {
-                if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(
+                        this@MainActivity,
+                        Manifest.permission.CAMERA
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
                     // 카메라 Permission 이 있을 경우!
                     startCamera()
                     camera_capture_button.visibility = View.VISIBLE
@@ -63,23 +70,40 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun requestPermission() {
-        if(ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity, Manifest.permission.CAMERA)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this@MainActivity,
+                Manifest.permission.CAMERA
+            )
+        ) {
+            // 카메라 권한 요구
             Toast.makeText(this@MainActivity, "카메라 권한이 요구됩니다.", Toast.LENGTH_SHORT).show()
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_CODE
+            )
         } else {
             // 다시는 보지 않기를 클릭하고 나서 요청을 보낼때!
             Toast.makeText(this@MainActivity, "카메라 허가를 받을 수 없습니다.", Toast.LENGTH_SHORT).show()
-            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_CODE
+            )
         }
     }
 
+    /**
+     * 요청을 받고 나서 들어오는 Method
+     */
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if(requestCode == CAMERA_PERMISSION_CODE) {
-            if(grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            // 권한요청을 한 갯수가 1개이고, grantResult 결과가 GRANTED -> 허용일 경우 카메라를 켜준다.
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 startCamera()
                 camera_capture_button.visibility = View.VISIBLE
                 viewFinder.visibility = View.VISIBLE
@@ -91,22 +115,72 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    private fun takePhoto() {}
+    private fun takePhoto() {
+
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time-stamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(
+                FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            })
+    }
 
     private fun startCamera() {
 
+        // CameraX 에서는 ViewFinder 가 촬영할 사진을 미리 볼 수 있는 역할을 한다. PreView Class 이용
+
+        // ProcessCameraProvider
+        // Camera 의 생명주기를 Activity 와 같은 LifeCycleOwner 의 생명주기에 Binding 시키는 것
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
+        // fun ListenableFuture.addListener(runnable: Runnable, executor: Executor)
+        // MainThread 에서 작동해야 하기 때문에 끝에 ContextCompat.getMainExecutor(this) 붙여준다.!!
         cameraProviderFuture.addListener(Runnable {
-            // Used to bind the lifecycle of cameras to the lifecycle owner
+
+            // 카메라의 수명주기 LifecycleOwner 애플리케이션의 프로세스 내에서 바인딩하는데 사용한다.
+            // ProcessCameraProvider -> 생명주기에 Binding 하는 객체 가져오기
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-            // Preview
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(viewFinder.surfaceProvider)
-                }
+
+            // Preview -> 카메라 미리보기를 구현해주는 것 , Builder 패턴 구현
+            // 개체를 초기화하고 빌드를 호출하고 뷰 파인더에서 표면 공급자를 가져온 다음 미리보기에서 설정한다.
+            val preview = Preview.Builder().build().also {
+                // setSurfaceProvider 함수 -> PreviewView 에 SurfaceProvider 제공
+                it.setSurfaceProvider(viewFinder.surfaceProvider)
+            }
+            /**
+             * SurfaceProvider 는 이미지 데이터를 받을 준비가 되었다는 신호를 카메라에게 보내주는 역할을 맡는다.
+             * 매개변수 중에서 Executor 가 따로 설정되어있지 않으면 MainThread 에서 SurfaceProvider 를 제공한다.
+             * 만약 null 값으로 Provider를 제거하면 카메라는 Preview 객체에서 이미지를 만드는 것을 멈춘다.
+             */
+
             // Select back camera as a default
+            // 후면 카메라로 Default 설정
+            // DEFAULT_FRONT_CAMERA -> 정면 카메라, DEFAULT_BACK_CAMERA -> 후면 카메라
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
             try {
                 // Unbind use cases before rebinding
