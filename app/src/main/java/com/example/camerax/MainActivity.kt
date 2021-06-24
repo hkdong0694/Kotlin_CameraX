8da5cc9c552a3d1211dc2657e5d953bcd4fb6599
한경동 <hkd0694@gmail.com>
2021-06-24 21:14:35 +0900

    이미지 분석 적용

--- app/src/main/java/com/example/camerax/MainActivity.kt
+++ app/src/main/java/com/example/camerax/MainActivity.kt
@@ -3,15 +3,13 @@ package com.example.camerax
 import android.Manifest
 import android.content.pm.PackageManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Size
 import android.view.View
 import android.widget.Toast
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -21,6 +19,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -42,8 +41,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         camera_capture_button.setOnClickListener(this)
 
         outputDirectory = getOutputDirectory()
-
-        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     override fun onClick(v: View?) {
@@ -55,6 +52,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     ) == PackageManager.PERMISSION_GRANTED
                 ) {
                     // 카메라 Permission 이 있을 경우!
+                    // SingleThread
+                    cameraExecutor = Executors.newSingleThreadExecutor()
                     startCamera()
                     camera_capture_button.visibility = View.VISIBLE
                     viewFinder.visibility = View.VISIBLE
@@ -149,7 +148,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     val savedUri = Uri.fromFile(photoFile)
                     val msg = "Photo capture succeeded: $savedUri"
                     Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
+                    // 이미지를 캡처 후 Preview 화면 Gone 처리!
+                    camera_capture_button.visibility = View.GONE
+                    viewFinder.visibility = View.GONE
+                    btn_camera.visibility = View.VISIBLE
+                    cameraExecutor.shutdown()
                 }
             })
     }
@@ -185,6 +188,37 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             // Image Capture
             imageCapture = ImageCapture.Builder().build()
 
+            // 이미지 분석용
+            // 이미지 분석은 차단 비차단 모드 ( 2가지 기능이 있음 )
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setTargetResolution(Size(1280, 720))
+                /**
+                 * 1. ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST -> 비차단 모드 ( 이 모드에서 실행자는 analyze() 메서드가 호출되는 시점에 카메라에서 마지막으로 사용 가능한 프레임을 수신합니다. )
+                 *    analyze() 메서드의 현재 프레임 속도가 단일 프레임의 지연 시간보다 느린 경우 analyze()가
+                 *    다음번에 데이터를 수신할 때 카메라 파이프라인에서 사용 가능한 최신 프레임을 가져오도록 몇몇 프레임을 건너뛸 수 있습니다.
+                 * 2. ImageAnalysis.STRATEGY_BLOCK_PRODUCER -> 차단 모드 ( 카메라에서 전송되는 프레임을 순차적으로 가져온다 )
+                 *    이는 analyze() 메서드가 현재 프레임 속도에서 단일 프레임의 지연 시간보다 오래 걸리면 메서드가
+                 *    반환할 때까지 새 프레임이 파이프라인에 진입하지 못하게 차단되므로
+                 *    프레임이 더 이상 최신 상태가 아닐 수 있음을 뜻합니다.
+                 */
+                // .setBackpressureStrategy(ImageAnalysis.STRATEGY_BLOCK_PRODUCER)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+
+            // 쉽게 정리하면
+            // STRATEGY_KEEP_ONLY_LATEST -> 이미지를 close 함으로써 가장 최신의 이미지를 가지고온다. ( 몇개 건너 뛸수도 있음 )
+            // STRATEGY_BLOCK_PRODUCER -> 이미지를 무조건 순차적으로 가져온다 ( 건너 뛰기 x )
+
+            // 제공되는 이미지 형식 ImageFormat.YUV_420_888
+            imageAnalysis.setAnalyzer(cameraExecutor, { image ->
+                // 이미지 분석!! 시작
+                val rotationDegrees = image.imageInfo.rotationDegrees
+                Log.d("asd", "들어와!! $rotationDegrees")
+
+                // 이걸 안쓰면 화면 멈춤! ( 이미지를 닫아 줘야 다음 프레임을 가져오기때문에 꼭 해줘야함! )
+                image.close()
+            })
+
             // Select back camera as a default
             // 후면 카메라로 Default 설정
             // DEFAULT_FRONT_CAMERA -> 정면 카메라, DEFAULT_BACK_CAMERA -> 후면 카메라
@@ -195,7 +229,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 // Bind use cases to camera
                 cameraProvider.unbindAll()
                 // Preview Binding!!
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+                // cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis, imageCapture)
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
